0ff219a671369ea84e0c7e0f8619be46e09827db
talizu <talissonsousa.social@gmail.com>
2025-05-23 18:32:10 +0000

    feat:pre solução

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// Escreva seu código aqui!
+//1. Implemente elementos da sala de aula como variáveis em TypeScript com tipos: `number`, `string`, `boolean`, `undefined` e objeto. Exiba as variáveis no terminal.
+
+import { readSync } from "node:fs";
+
+let Alunos: number;
+Alunos=12;
+console.log('há '+ Alunos + ' na sala');
+
+let AR : string;
+readSync
+console.log('há '+ Alunos + ' na sala');

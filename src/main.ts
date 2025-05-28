a5abd32379c54cbb5a93bc7842294f4ebdc82c5a
talizu <talissonsousa.social@gmail.com>
2025-05-28 01:48:54 +0000

    Resolução da atividade de 'FUP'

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 //1. Implemente elementos da sala de aula como variáveis em TypeScript com tipos: `number`, `string`, `boolean`, `undefined` e objeto. Exiba as variáveis no terminal.
 
-import { readSync } from "node:fs";
+//Implemente elementos da sala de aula como variáveis em TypeScript com tipos: number, string, boolean, undefined e objeto. Exiba as variáveis no terminal.
 
-let Alunos: number;
-Alunos=12;
-console.log('há '+ Alunos + ' na sala');
+const Janelas :{
+    Quantidade: number;
+}={ Quantidade: 6, }
 
-let AR : string;
-readSync
-console.log('há '+ Alunos + ' na sala');
+const Professora :{ 
+nome: string;
+}={ nome:"Lana", }
+
+const Arcondicionado:{
+   Funcionando: boolean;
+}={ Funcionando: true, }
+
+let soninho;
+
+// console.log(" Na sala há:"+ Janelas.Quantidade + " A aula é ministrada pela professora:" + Professora.nome + "nesse momento o Ar condicionado está:" + Arcondicionado.Funcionando, + " o presente aluno está:" + soninho );
+
+console.log(`
+    Na sala há ${Janelas.Quantidade} janelas\n
+    A aula é ministrada pela professora ${Professora.nome}\n
+    Nesse momento o ar condicionado está ${Arcondicionado.Funcionando == true ? "Ligadinho" : "Desligadinho"}\n
+    Tô me sentindo meio... ${soninho}
+`);
+
+//Short IF -> condição? se verdadeiro : se falso
